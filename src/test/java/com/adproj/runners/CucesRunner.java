1348e7b505da46637cc2dc4b58fcfe420d7b002b
Rasim Aghayev <rasim.ag1983@gmail.com>
2021-07-12 22:10:40 -0400

    Initial commit

---
+++ src/test/java/com/adproj/runners/CucesRunner.java
@@ -0,0 +1,19 @@
+package com.adproj.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
+        features = "src/test/resources/features",
+        glue = "com/adproj/step_definitions",
+        dryRun = true
+
+
+)
+
+public class CucesRunner {
+
+}
